8e11741d00a67c470857712fca9bdc0bfedb4c34
huangbin <1127779503@qq.com>
2022-10-26 16:40:17 +0800

    更改调试环境

--- vue/jest.config.js
+++ vue/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['./scripts/setupJestEnv.ts'],
+  testEnvironment: "jsdom",
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["./scripts/setupJestEnv.ts"],
   globals: {
-    __DEV__: true,
+    __DEV__: false,
     __TEST__: true,
-    __VERSION__: require('./package.json').version,
+    __VERSION__: require("./package.json").version,
     __BROWSER__: false,
     __GLOBAL__: false,
     __ESM_BUNDLER__: true,
@@ -16,47 +16,47 @@ module.exports = {
     __FEATURE_SUSPENSE__: true,
     __FEATURE_PROD_DEVTOOLS__: false,
     __COMPAT__: true,
-    'ts-jest': {
+    "ts-jest": {
       tsconfig: {
-        target: 'esnext',
-        sourceMap: true
-      }
-    }
+        target: "esnext",
+        sourceMap: true,
+      },
+    },
   },
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'lcov', 'text'],
+  coverageDirectory: "coverage",
+  coverageReporters: ["html", "lcov", "text"],
   collectCoverageFrom: [
-    'packages/*/src/**/*.ts',
-    '!packages/runtime-test/src/utils/**',
-    '!packages/template-explorer/**',
-    '!packages/sfc-playground/**',
-    '!packages/size-check/**',
-    '!packages/runtime-core/src/profiling.ts',
-    '!packages/runtime-core/src/customFormatter.ts',
+    "packages/*/src/**/*.ts",
+    "!packages/runtime-test/src/utils/**",
+    "!packages/template-explorer/**",
+    "!packages/sfc-playground/**",
+    "!packages/size-check/**",
+    "!packages/runtime-core/src/profiling.ts",
+    "!packages/runtime-core/src/customFormatter.ts",
     // DOM transitions are tested via e2e so no coverage is collected
-    '!packages/runtime-dom/src/components/Transition*',
+    "!packages/runtime-dom/src/components/Transition*",
     // only called in browsers
-    '!packages/vue/src/devCheck.ts',
+    "!packages/vue/src/devCheck.ts",
     // only used as a build entry
-    '!packages/vue/src/runtime.ts',
+    "!packages/vue/src/runtime.ts",
     // mostly just entries
-    '!packages/vue-compat/**'
+    "!packages/vue-compat/**",
   ],
-  watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  watchPathIgnorePatterns: ["/node_modules/", "/dist/", "/.git/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
   moduleNameMapper: {
-    '@vue/consolidate': '@vue/consolidate',
-    '@vue/compat': '<rootDir>/packages/vue-compat/src',
-    '^@vue/(.*?)$': '<rootDir>/packages/$1/src',
-    'vue/compiler-sfc': '<rootDir>/packages/compiler-sfc/src',
-    'vue/server-renderer': '<rootDir>/packages/server-renderer/src',
-    vue: '<rootDir>/packages/vue/src'
+    "@vue/consolidate": "@vue/consolidate",
+    "@vue/compat": "<rootDir>/packages/vue-compat/src",
+    "^@vue/(.*?)$": "<rootDir>/packages/$1/src",
+    "vue/compiler-sfc": "<rootDir>/packages/compiler-sfc/src",
+    "vue/server-renderer": "<rootDir>/packages/server-renderer/src",
+    vue: "<rootDir>/packages/vue/src",
   },
   rootDir: __dirname,
-  testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)'],
+  testMatch: ["<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)"],
   testPathIgnorePatterns: process.env.SKIP_E2E
     ? // ignore example tests on netlify builds since they don't contribute
       // to coverage and can cause netlify builds to fail
-      ['/node_modules/', '/examples/__tests__']
-    : ['/node_modules/']
-}
+      ["/node_modules/", "/examples/__tests__"]
+    : ["/node_modules/"],
+};
