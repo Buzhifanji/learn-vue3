d7ec0e541136e04f517408f51bbb5801bd01ecf0
huangbin <1127779503@qq.com>
2022-10-26 13:55:19 +0800

    添加 createGetter 阅读笔记

--- vue/packages/reactivity/src/baseHandlers.ts
+++ vue/packages/reactivity/src/baseHandlers.ts
@@ -6,16 +6,16 @@ import {
   isIntegerKey,
   isObject,
   isSymbol,
-  makeMap
-} from '@vue/shared'
+  makeMap,
+} from "@vue/shared";
 import {
   ITERATE_KEY,
   pauseTracking,
   resetTracking,
   track,
-  trigger
-} from './effect'
-import { TrackOpTypes, TriggerOpTypes } from './operations'
+  trigger,
+} from "./effect";
+import { TrackOpTypes, TriggerOpTypes } from "./operations";
 import {
   isReadonly,
   isShallow,
@@ -27,12 +27,12 @@ import {
   shallowReactiveMap,
   shallowReadonlyMap,
   Target,
-  toRaw
-} from './reactive'
-import { isRef } from './ref'
-import { warn } from './warning'
+  toRaw,
+} from "./reactive";
+import { isRef } from "./ref";
+import { warn } from "./warning";
 
-const isNonTrackableKeys = /*#__PURE__*/ makeMap(`__proto__,__v_isRef,__isVue`)
+const isNonTrackableKeys = /*#__PURE__*/ makeMap(`__proto__,__v_isRef,__isVue`);
 
 const builtInSymbols = new Set(
   /*#__PURE__*/
@@ -40,24 +40,24 @@ const builtInSymbols = new Set(
     // ios10.x Object.getOwnPropertyNames(Symbol) can enumerate 'arguments' and 'caller'
     // but accessing them on Symbol leads to TypeError because Symbol is a strict mode
     // function
-    .filter(key => key !== 'arguments' && key !== 'caller')
-    .map(key => (Symbol as any)[key])
+    .filter((key) => key !== "arguments" && key !== "caller")
+    .map((key) => (Symbol as any)[key])
     .filter(isSymbol)
-)
+);
 
-const get = /*#__PURE__*/ createGetter()
-const shallowGet = /*#__PURE__*/ createGetter(false, true)
-const readonlyGet = /*#__PURE__*/ createGetter(true)
-const shallowReadonlyGet = /*#__PURE__*/ createGetter(true, true)
+const get = /*#__PURE__*/ createGetter();
+const shallowGet = /*#__PURE__*/ createGetter(false, true);
+const readonlyGet = /*#__PURE__*/ createGetter(true);
+const shallowReadonlyGet = /*#__PURE__*/ createGetter(true, true);
 
-const arrayInstrumentations = /*#__PURE__*/ createArrayInstrumentations()
+const arrayInstrumentations = /*#__PURE__*/ createArrayInstrumentations();
 
 /**
  * 处理响应式数组：重写数组'includes', 'indexOf', 'lastIndexOf'，'push', 'pop', 'shift', 'unshift', 'splice'七个api
  *
  */
 function createArrayInstrumentations() {
-  const instrumentations: Record<string, Function> = {}
+  const instrumentations: Record<string, Function> = {};
   // instrument identity-sensitive Array methods to account for possible reactive
   // values
 
@@ -66,37 +66,45 @@ function createArrayInstrumentations() {
    *  这里需要注意 这三个api用法里的第二个参数，由于这个参数存在，可能会导致数组遍历不是索引0开始，在这种情况下，就会出现数组中的元素不是响应式。
    *  所以为了全部数组中的数据都是响应式的，从头遍历一次数据。
    */
-  ;(['includes', 'indexOf', 'lastIndexOf'] as const).forEach(key => {
+  (["includes", "indexOf", "lastIndexOf"] as const).forEach((key) => {
     instrumentations[key] = function (this: unknown[], ...args: unknown[]) {
       // 获取数组数据
-      const arr = toRaw(this) as any
+      const arr = toRaw(this) as any;
       // 遍历数组，并收集 get 依赖
       for (let i = 0, l = this.length; i < l; i++) {
-        track(arr, TrackOpTypes.GET, i + '')
+        track(arr, TrackOpTypes.GET, i + "");
       }
       // we run the method using the original args first (which may be reactive)
       // 执行 查找
-      const res = arr[key](...args)
+      const res = arr[key](...args);
       if (res === -1 || res === false) {
         // if that didn't work, run it again using raw values.
         // 如果没有找到，就尝试把参数转变成 原始数据，重新查找一次
-        return arr[key](...args.map(toRaw))
+        return arr[key](...args.map(toRaw));
       } else {
-        return res
+        return res;
       }
-    }
-  })
+    };
+  });
   // instrument length-altering mutation methods to avoid length being tracked
   // which leads to infinite loops in some cases (#2137)
-  ;(['push', 'pop', 'shift', 'unshift', 'splice'] as const).forEach(key => {
+
+  /**
+   * 'push', 'pop', 'shift', 'unshift', 'splice' 会改变数组，重写为了确保修改后的数组也是响应式的
+   *
+   */
+  (["push", "pop", "shift", "unshift", "splice"] as const).forEach((key) => {
     instrumentations[key] = function (this: unknown[], ...args: unknown[]) {
-      pauseTracking()
-      const res = (toRaw(this) as any)[key].apply(this, args)
-      resetTracking()
-      return res
-    }
-  })
-  return instrumentations
+      // 开启 依赖收集
+      pauseTracking();
+      // 执行 数组api方法
+      const res = (toRaw(this) as any)[key].apply(this, args);
+      // 执行完后重置依赖收集
+      resetTracking();
+      return res;
+    };
+  });
+  return instrumentations;
 }
 
 /**
@@ -110,13 +118,13 @@ function createGetter(isReadonly = false, shallow = false) {
     // 处理 key 为 ReactiveFlags 特殊标识符 的情况
     if (key === ReactiveFlags.IS_REACTIVE) {
       // 是不是reactive
-      return !isReadonly
+      return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       //是不是只读的
-      return isReadonly
+      return isReadonly;
     } else if (key === ReactiveFlags.IS_SHALLOW) {
       // 是不是浅层响应式
-      return shallow
+      return shallow;
     } else if (
       // 原始值
       key === ReactiveFlags.RAW &&
@@ -131,48 +139,58 @@ function createGetter(isReadonly = false, shallow = false) {
           : reactiveMap
         ).get(target)
     ) {
-      return target
+      return target;
     }
 
-    const targetIsArray = isArray(target)
+    const targetIsArray = isArray(target);
 
     // 处理 数组
     if (!isReadonly && targetIsArray && hasOwn(arrayInstrumentations, key)) {
-      return Reflect.get(arrayInstrumentations, key, receiver)
+      return Reflect.get(arrayInstrumentations, key, receiver);
     }
 
-    const res = Reflect.get(target, key, receiver)
+    // 通过 Reflect.get 读取对象属性
+    const res = Reflect.get(target, key, receiver);
 
+    //  不拦截内置Symbol属性和__proto__,__v_isRef和__isVue属性
     if (isSymbol(key) ? builtInSymbols.has(key) : isNonTrackableKeys(key)) {
-      return res
+      return res;
     }
 
     if (!isReadonly) {
-      track(target, TrackOpTypes.GET, key)
+      // 收集 target对象 key对应的依赖
+      track(target, TrackOpTypes.GET, key);
     }
 
+    // 如果是创建 shallowReactive、或是 shallowReadonly，转换就到此结束了，就不会继续处理嵌套数据了
     if (shallow) {
-      return res
+      return res;
     }
 
     if (isRef(res)) {
       // ref unwrapping - skip unwrap for Array + integer key.
-      return targetIsArray && isIntegerKey(key) ? res : res.value
+
+      // 处理 reactivity 属性值 为 Ref 实例
+      // eg: const name = 'v', info = reactive({name})
+      // console.log(info.name) // v
+      return targetIsArray && isIntegerKey(key) ? res : res.value;
     }
 
     if (isObject(res)) {
       // Convert returned value into a proxy as well. we do the isObject check
       // here to avoid invalid value warning. Also need to lazy access readonly
       // and reactive here to avoid circular dependency.
-      return isReadonly ? readonly(res) : reactive(res)
+      // 如果 属性值 是个对象，继续把 属性值 res 转换成响应式对象
+      // 这里惰性转换，只有读取的res 的时候，会触发转换
+      return isReadonly ? readonly(res) : reactive(res);
     }
 
-    return res
-  }
+    return res;
+  };
 }
 
-const set = /*#__PURE__*/ createSetter()
-const shallowSet = /*#__PURE__*/ createSetter(true)
+const set = /*#__PURE__*/ createSetter();
+const shallowSet = /*#__PURE__*/ createSetter(true);
 
 function createSetter(shallow = false) {
   return function set(
@@ -181,18 +199,18 @@ function createSetter(shallow = false) {
     value: unknown,
     receiver: object
   ): boolean {
-    let oldValue = (target as any)[key]
+    let oldValue = (target as any)[key];
     if (isReadonly(oldValue) && isRef(oldValue) && !isRef(value)) {
-      return false
+      return false;
     }
     if (!shallow) {
       if (!isShallow(value) && !isReadonly(value)) {
-        oldValue = toRaw(oldValue)
-        value = toRaw(value)
+        oldValue = toRaw(oldValue);
+        value = toRaw(value);
       }
       if (!isArray(target) && isRef(oldValue) && !isRef(value)) {
-        oldValue.value = value
-        return true
+        oldValue.value = value;
+        return true;
       }
     } else {
       // in shallow mode, objects are set as-is regardless of reactive or not
@@ -201,41 +219,41 @@ function createSetter(shallow = false) {
     const hadKey =
       isArray(target) && isIntegerKey(key)
         ? Number(key) < target.length
-        : hasOwn(target, key)
-    const result = Reflect.set(target, key, value, receiver)
+        : hasOwn(target, key);
+    const result = Reflect.set(target, key, value, receiver);
     // don't trigger if target is something up in the prototype chain of original
     if (target === toRaw(receiver)) {
       if (!hadKey) {
-        trigger(target, TriggerOpTypes.ADD, key, value)
+        trigger(target, TriggerOpTypes.ADD, key, value);
       } else if (hasChanged(value, oldValue)) {
-        trigger(target, TriggerOpTypes.SET, key, value, oldValue)
+        trigger(target, TriggerOpTypes.SET, key, value, oldValue);
       }
     }
-    return result
-  }
+    return result;
+  };
 }
 
 function deleteProperty(target: object, key: string | symbol): boolean {
-  const hadKey = hasOwn(target, key)
-  const oldValue = (target as any)[key]
-  const result = Reflect.deleteProperty(target, key)
+  const hadKey = hasOwn(target, key);
+  const oldValue = (target as any)[key];
+  const result = Reflect.deleteProperty(target, key);
   if (result && hadKey) {
-    trigger(target, TriggerOpTypes.DELETE, key, undefined, oldValue)
+    trigger(target, TriggerOpTypes.DELETE, key, undefined, oldValue);
   }
-  return result
+  return result;
 }
 
 function has(target: object, key: string | symbol): boolean {
-  const result = Reflect.has(target, key)
+  const result = Reflect.has(target, key);
   if (!isSymbol(key) || !builtInSymbols.has(key)) {
-    track(target, TrackOpTypes.HAS, key)
+    track(target, TrackOpTypes.HAS, key);
   }
-  return result
+  return result;
 }
 
 function ownKeys(target: object): (string | symbol)[] {
-  track(target, TrackOpTypes.ITERATE, isArray(target) ? 'length' : ITERATE_KEY)
-  return Reflect.ownKeys(target)
+  track(target, TrackOpTypes.ITERATE, isArray(target) ? "length" : ITERATE_KEY);
+  return Reflect.ownKeys(target);
 }
 
 export const mutableHandlers: ProxyHandler<object> = {
@@ -243,8 +261,8 @@ export const mutableHandlers: ProxyHandler<object> = {
   set,
   deleteProperty,
   has,
-  ownKeys
-}
+  ownKeys,
+};
 
 export const readonlyHandlers: ProxyHandler<object> = {
   get: readonlyGet,
@@ -253,29 +271,29 @@ export const readonlyHandlers: ProxyHandler<object> = {
       warn(
         `Set operation on key "${String(key)}" failed: target is readonly.`,
         target
-      )
+      );
     }
-    return true
+    return true;
   },
   deleteProperty(target, key) {
     if (__DEV__) {
       warn(
         `Delete operation on key "${String(key)}" failed: target is readonly.`,
         target
-      )
+      );
     }
-    return true
-  }
-}
+    return true;
+  },
+};
 
 export const shallowReactiveHandlers = /*#__PURE__*/ extend(
   {},
   mutableHandlers,
   {
     get: shallowGet,
-    set: shallowSet
+    set: shallowSet,
   }
-)
+);
 
 // Props handlers are special in the sense that it should not unwrap top-level
 // refs (in order to allow refs to be explicitly passed down), but should
@@ -284,6 +302,6 @@ export const shallowReadonlyHandlers = /*#__PURE__*/ extend(
   {},
   readonlyHandlers,
   {
-    get: shallowReadonlyGet
+    get: shallowReadonlyGet,
   }
-)
+);
